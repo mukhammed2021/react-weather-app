c57a31a776fe57f9e5b0a905060c86097ee9d1af
Mukhammed <massalimov.mukhammed@mail.ru>
2024-10-09 12:30:58 +0500

    add responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
    return (
-      <footer className="text-center">
+      <footer className="text-center max-[375px]:text-sm">
          Designed and developed by Mukhammed
       </footer>
    );
