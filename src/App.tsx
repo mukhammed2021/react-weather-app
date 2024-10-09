359cec00753b7b2f5a197cede42b7da897b24c18
Mukhammed <massalimov.mukhammed@mail.ru>
2024-10-09 12:31:45 +0500

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
-export default function App() {
-   return <div>App</div>;
-}
+import Footer from "./components/Footer";
+import WeatherCard from "./components/WeatherCard";
 
+export default function App() {
+   return (
+      <div className="mx-auto flex min-h-svh max-w-2xl flex-col justify-center p-4">
+         <div className="mb-3 flex grow flex-col justify-center">
+            <WeatherCard />
+         </div>
+         <Footer />
+      </div>
+   );
+}
