b689dc2516db4a9de52f695ec635e08fdf9c706c
Mukhammed <massalimov.mukhammed@mail.ru>
2024-10-09 15:36:42 +0500

    change theme

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,38 @@
+import { useToggleTheme } from "../hooks/useToggleTheme";
+
 export default function Header() {
+   const { theme, handleTheme } = useToggleTheme();
+
    return (
       <header className="mb-11 flex items-center gap-1 max-[520px]:mb-9 max-[425px]:mb-6">
-         <button type="button">
-            <svg className="size-6">
-               <use xlinkHref="#dark" />
-            </svg>
+         <button type="button" onClick={handleTheme}>
+            {theme === "dark" ? (
+               <Icon xlinkHref="light" />
+            ) : (
+               <Icon xlinkHref="dark" />
+            )}
          </button>
          <form className="grow">
             <input
                type="search"
                placeholder="Введите город"
                autoComplete="off"
-               className="block w-full appearance-none rounded-lg bg-transparent px-3 py-2 placeholder:text-slate-400"
+               className="block w-full appearance-none rounded-lg bg-transparent px-3 py-2 placeholder:text-slate-400 dark:placeholder:text-slate-300"
             />
          </form>
          <button type="button">
-            <svg className="size-6">
-               <use xlinkHref="#search" />
-            </svg>
+            <Icon xlinkHref="search" />
          </button>
       </header>
+   );
+}
+
+type IconPros = React.ComponentPropsWithoutRef<"use">;
+
+function Icon({ xlinkHref }: IconPros) {
+   return (
+      <svg className="size-6">
+         <use xlinkHref={`#${xlinkHref}`} />
+      </svg>
    );
 }
