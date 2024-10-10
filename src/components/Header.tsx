5a00b18106e1dbd427fa44645edca635ae9bfa1f
Mukhammed <massalimov.mukhammed@mail.ru>
2024-10-10 12:10:48 +0500

    handleWeather function

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,8 +8,28 @@ type HeaderProps = {
    setWeatherData: React.Dispatch<React.SetStateAction<weatherDataProps>>;
 };
 
-export default function Header({ query, setQuery, setWeatherData }: HeaderProps) {
+export default function Header({
+   query,
+   setQuery,
+   setWeatherData,
+}: HeaderProps) {
    const { theme, handleTheme } = useToggleTheme();
+
+   function handleWeather() {
+      async function coords() {
+         const resCoords = await fetch(
+            `https://nominatim.openstreetmap.org/search?format=json&city=${query}&limit=1`,
+         );
+         const dataCoords = await resCoords.json();
+
+         const res = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${dataCoords[0]?.lat}&lon=${dataCoords[0]?.lon}&appid=${API_KEY}&units=metric&lang=ru`,
+         );
+         const data = await res.json();
+         setWeatherData(data);
+      }
+      coords();
+   }
 
    return (
       <header className="mb-11 flex items-center gap-1 max-[520px]:mb-9 max-[425px]:mb-6">
@@ -23,20 +43,7 @@ export default function Header({ query, setQuery, setWeatherData }: HeaderProps)
          <form
             onSubmit={(e) => {
                e.preventDefault();
-
-               async function coords() {
-                  const resCoords = await fetch(
-                     `https://nominatim.openstreetmap.org/search?format=json&city=${query}&limit=1`,
-                  );
-                  const dataCoords = await resCoords.json();
-
-                  const res = await fetch(
-                     `https://api.openweathermap.org/data/2.5/weather?lat=${dataCoords[0]?.lat}&lon=${dataCoords[0]?.lon}&appid=${API_KEY}&units=metric&lang=ru`,
-                  );
-                  const data = await res.json();
-                  setWeatherData(data);
-               }
-               coords();
+               handleWeather();
             }}
             className="grow"
          >
@@ -49,7 +56,7 @@ export default function Header({ query, setQuery, setWeatherData }: HeaderProps)
                className="block w-full appearance-none rounded-lg bg-transparent px-3 py-2 placeholder:text-slate-400 dark:placeholder:text-slate-300"
             />
          </form>
-         <button type="button">
+         <button type="button" onClick={handleWeather}>
             <Icon xlinkHref="search" />
          </button>
       </header>
@@ -60,7 +67,7 @@ type IconPros = React.ComponentPropsWithoutRef<"use">;
 
 function Icon({ xlinkHref }: IconPros) {
    return (
-      <svg className="size-6">
+      <svg className="size-6 rounded transition-colors hover:bg-slate-300 dark:hover:bg-slate-700">
          <use xlinkHref={`#${xlinkHref}`} />
       </svg>
    );
