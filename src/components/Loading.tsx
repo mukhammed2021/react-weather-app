d7560e0ac85bab8bf0f4f8935a4a3b4b071e1a5a
Mukhammed <massalimov.mukhammed@mail.ru>
2024-10-10 15:16:28 +0500

    loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+   return (
+      <div className="flex h-[468px] flex-col items-center justify-center max-[520px]:h-[396px] max-[425px]:h-[344px]">
+         Загрузка...
+      </div>
+   );
+}
